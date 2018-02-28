de7b7fcd20665acea455993dec82d70110f2177a
Malfoy <antoine.limasset@gmail.com>
2018-02-28 16:16:59 +0100

    forgot file

---
+++ src/utils/fq2fa.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+
+
+using namespace std;
+
+
+
+int main(int argc, char ** argv){
+	if(argc<2){
+		cout<<"[Fasta file]"<<endl;
+		exit(0);
+	}
+	string input(argv[1]);
+	srand (time(NULL));
+	string ref, useless,header;
+	ifstream in(input);
+	vector<uint> lengths;
+	while(not in.eof()){
+		getline(in,header);
+		getline(in,ref);
+		getline(in,useless);
+		getline(in,useless);
+		if(ref.size()>1){
+			cout<<">"+header<<endl;
+			cout<<ref<<endl;
+		}
+	}
+}
