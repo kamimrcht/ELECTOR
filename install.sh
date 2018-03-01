80d228f3dbbc61f9eab8d32932584bb1f8cf61f9
Malfoy <antoine.limasset@gmail.com>
2018-03-01 17:14:26 +0100

    better instalation script for cluster

--- install.sh
+++ install.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
 mkdir bin;
+
 cd src/simulator;
 make;
 mv simulator ../../bin;
+
 cd ../poa-graph;
 make poa;
 mv poa ../../bin;
+
 cd ../utils;
 make;
 mv fq2fa ../../bin;
+
 cd ../../minimap2;
 make;
 cd ../miniasm;
 make;
+
 cd ../bwa;
 make;
+
 cd ../htslib;
 autoheader;
 autoconf;
-./configure;
+./configure --disable-lzma;
 make;
+
 cd ../samtools;
 autoheader:
 autoconf -Wno-syntax;
-./configure;
+./configure --disable-lzma;
 make;
+
 cd ../src/split;
 make;
 mv masterSplitter ../../bin;
