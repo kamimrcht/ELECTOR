9dad8636a03903ec6ba9b6f15ec11c62ecd22a75
Pierre-Morisse <pierre.morisse2@univ-rouen.fr>
2018-12-19 15:59:39 +0100

    Modify number of reads by file, from 1k to 500

--- alignment.py
+++ alignment.py
@@ -90,7 +90,7 @@ def getPOA(corrected, reference, uncorrected, threads, installDirectory, outDir,
 		return 0, 0
 	else:
 		#amount_nuc=100*1000*1000;
-		amount_read=1000*100;
+		amount_read=1000*50;
 		print("- Means that a large amount of nuc has been handled: "+str(amount_read))
 		global installDirectoryGlobal
 		installDirectoryGlobal=installDirectory
