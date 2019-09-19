64aa1531f633c7b5a54ead276027774854144737
Pierre-Morisse <pierre.morisse2@univ-rouen.fr>
2019-09-19 21:05:49 +0200

    Update LoRDEC correction scripts

--- reproduce_manuscript_results/correction/LoRDEC/corEcoli.sh
+++ reproduce_manuscript_results/correction/LoRDEC/corEcoli.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-lordec-correct -T $(nproc) -i ../../longReads/simEcoli.fasta -2 ../../shortReads/Ecoli.fa -k 19 -o LordecEcoli.fasta -s 3
+lordec-correct -T $(nproc) -i ../../longReads/simEcoli.fasta -2 ../../shortReads/simEcoli.fa -k 19 -o LordecEcoli.fasta -s 3
 lordec-trim-split -i LordecEcoli.fasta -o LordecEcoli.split.fasta
