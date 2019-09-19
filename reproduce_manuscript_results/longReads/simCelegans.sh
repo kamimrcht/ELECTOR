26ba9a49b0bc3d98d9a451529a66a04a2042a6f8
Pierre-Morisse <pierre.morisse2@univ-rouen.fr>
2019-09-19 22:49:31 +0200

    Update simulation scripts

--- reproduce_manuscript_results/longReads/simCelegans.sh
+++ reproduce_manuscript_results/longReads/simCelegans.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source activate simlord
-simlord --read-reference ../references/Celegans.fasta -t 0.305 -pi 0.22 -pd 0.08 -ps 0.01 --coverage 20 simCelegans
+simlord --read-reference ../references/Celegans.fasta -t 0.305 -pi 0.22 -pd 0.08 -ps 0.01 --coverage 20 --sam-output simCelegans.fastq.sam simCelegans
 ./../../bin/fq2fa simCelegans.fastq > simCelegans.fasta
 source deactivate
